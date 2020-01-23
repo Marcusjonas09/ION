27bb990c048619ce676754e91635c256277e19a1
mgcurioso <curiosomarcusjonas24@gmail.com>
2020-01-23 15:29:11 +0800

    edited_superadmin

--- application/views/content_super_admin/school-parameters/school-parameters.php
+++ application/views/content_super_admin/school-parameters/school-parameters.php
@@ -11,15 +11,12 @@
     <section class="content container-fluid">
 
         <div class="box box-success">
-
             <div class="box-header with-border">
                 <h3 class="box-title"><strong>Current Parameters</strong></h3>
             </div>
             <!-- /.box-header -->
-
             <form action="<?= base_url() ?>Student/changepass" method="post">
                 <div class="box-body">
-
                     <div class="form-group col-md-4">
                         <label>School Year</label>
                         <input type="text" disabled class="form-control" name="schoolyear" placeholder="School Year">
@@ -28,10 +25,84 @@
                         <label>School Term</label>
                         <input type="text" disabled class="form-control" name="schoolterm" placeholder="School Term">
                     </div>
-
                 </div>
             </form>
+            <!-- /.box-body -->
+        </div>
+        <div class="box box-success">
+            <div class="box-header with-border">
+                <h3 class="box-title"><strong>Set Parameters</strong></h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3 id="simul_number">0</h3>
 
+                            <p>College/s</p>
+                        </div>
+                        <div class="icon">
+                            <!-- <i class="fa fa-shopping-cart"></i> -->
+                        </div>
+                        <a href="<?= base_url() ?>Superadmin/college" class="small-box-footer">
+                            More info <i class="fa fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3 id="simul_number">0</h3>
+
+                            <p>Department/s</p>
+                        </div>
+                        <div class="icon">
+                            <!-- <i class="fa fa-shopping-cart"></i> -->
+                        </div>
+                        <a href="<?= base_url() ?>Superadmin/department" class="small-box-footer">
+                            More info <i class="fa fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3 id="simul_number">0</h3>
+
+                            <p>Program/s</p>
+                        </div>
+                        <div class="icon">
+                            <!-- <i class="fa fa-shopping-cart"></i> -->
+                        </div>
+                        <a href="<?= base_url() ?>Superadmin/programs" class="small-box-footer">
+                            More info <i class="fa fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3 id="simul_number">0</h3>
+
+                            <p>Curriculum</p>
+                        </div>
+                        <div class="icon">
+                            <!-- <i class="fa fa-shopping-cart"></i> -->
+                        </div>
+                        <a href="<?= base_url() ?>Superadmin/curriculum" class="small-box-footer">
+                            More info <i class="fa fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
             <!-- /.box-body -->
         </div>
 
